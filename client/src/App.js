73b45864872f41b68eb731e0e50b604274003c0f
Sami Bachir <samibachir20@email.com>
2019-06-05 10:20:40 -0400

    house cleaning

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          <Switch>
+            <Route path='/' exact component={ componentName }/>
+            <Route path='/create-restaurant' component={ CreateRestaurantPage }/>
+          </Switch>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;

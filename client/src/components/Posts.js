078b91cfce68e44e1a7140d1a388fc0cfc29fba1
Sami Bachir <samibachir20@email.com>
2019-06-07 15:59:15 -0400

    workin on update

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-// import UpdatePostPage from './UpdatePostPage';
-// import {Redirect} from 'react-router-dom';
-import './Posts.css'
+import './Posts.css';
 
 export default class Posts extends Component {
-  
+    constructor(props) {
+        super(props)
+      
+        this.state = {
+            title1: '',
+            genre1: '',
+            content1: '',
+            created: false,
+            clicked: false
+        }
+    }
+
+// Delete Function
 
     handleSubmit = (key) => {
     //    event.preventDefault();
@@ -18,13 +28,36 @@ export default class Posts extends Component {
         console.log(key)
     }
 
-    handleSubmitUpdate = (key) => {
-        axios.put(`http://localhost:3001/posts/${key}`)
+
+// Update Function
+
+    onPostFormChange = (event) => {
+        const { name, value } = event.target
+        this.setState({ [name]: value })
+    }
+
+
+
+    handleSubmitUpdate = async (key) => {
+        console.log(`Post Updated:`, this.state)
+        const newPost = {
+            "title1": this.state.title1,
+            "genre1": this.state.genre1,
+            "content1": this.state.content1
+        }
+
+        axios.put(`http://localhost:3001/posts/${key}`, newPost)
             .then(res => {
             console.log(res);
             console.log(res.data);
         })
         console.log(key)
+    }
+
+    handleSubmitRedirect = async () => {
+        await this.setState ({
+            clicked: true
+        })
     }
 
     componentDidMount(){
@@ -33,13 +66,72 @@ export default class Posts extends Component {
 
     render() {
 
-        // if(this.state.updated === true) {
-        //     return <Redirect to="/posts" />
-        // }
 
         let allPosts = this.props.allposts.map(post => {
             const key=post.id
             console.log(key)
+
+            if (this.state.clicked) {
+                return (
+                  <div>
+                    <div id="upload">
+                        <form onSubmit={this.handleSubmitUpdate(key)}>
+                            <div className="field">
+                                <label htmlFor="name">Title:</label>
+                                <div className="control">
+                                    <input 
+                                        className="input"
+                                        type="text"
+                                        placeholder="Title"
+                                        name="title1"
+                                        value={
+                                            this.state.title1
+                                        }
+                                        onChange={this.onPostFormChange}
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label htmlFor="name">Genre:</label>
+                                <div className="control">
+                                    <input 
+                                        className="input"
+                                        type="text"
+                                        placeholder="Genre"                              
+                                        name="genre1"
+                                        value={
+                                            this.state.genre1
+                                        }
+                                        onChange={this.onPostFormChange}
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label htmlFor="name">Content:</label>
+                                <div className="control">
+                                    <textarea 
+                                        className="input"
+                                        type="text"
+                                        placeholder="Content" 
+                                        name="content1"
+                                        value={
+                                            this.state.content1
+                                        }
+                                        onChange={this.onPostFormChange}
+                                        rows="15">
+                                    </textarea>
+                                </div>
+                            </div>
+                            <div className="field is-grouped">
+                                <div className="control">
+                                    <button className="button is-link" type="submit">Update</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                )
+              } else {
             return (
                 <article class="message is-info">
                     <div class="message-header">
@@ -50,10 +142,33 @@ export default class Posts extends Component {
                     <div class="message-body">
                         <p>{post.content}</p>
                     </div>
-                    <button onClick={()=>{this.handleSubmitUpdate(key)}}>UPDATE</button>
-                    {/* <button>UPDATE</button> */}
+
+                    <article className="media">
+                        <div className="media-content">
+                            <div className="field">
+                                <p className="control">
+                                    <textarea className="textarea" placeholder="Add a comment..." defaultValue={""} />
+                                </p>
+                            </div>
+                            <nav className="level">
+                                <div className="level-left">
+                                    <div className="level-item">
+                                        <a href="http://localhost:3000/posts" className="button is-info">Submit</a>
+                                    </div>
+                                </div>
+                                <div className="level-right">
+                                    <div className="level-item">
+                                        <div className="level-item">
+                                        <button className="button is-info" onClick={()=>{this.handleSubmitRedirect(key)}}>Update</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </article>
                 </article>
             )
+            }
         })
         return (
             <div className="posts">
@@ -62,90 +177,3 @@ export default class Posts extends Component {
         );
     }
 }
-
-// Comment stuff
-
-
-// <article class="media">
-//   <figure class="media-left">
-//     <p class="image is-64x64">
-//       <img src="https://bulma.io/images/placeholders/128x128.png">
-//     </p>
-//   </figure>
-//   <div class="media-content">
-//     <div class="content">
-//       <p>
-//         <strong>Barbara Middleton</strong>
-//         <br>
-//         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
-//         <br>
-//         <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
-//       </p>
-//     </div>
-
-//     <article class="media">
-//       <figure class="media-left">
-//         <p class="image is-48x48">
-//           <img src="https://bulma.io/images/placeholders/96x96.png">
-//         </p>
-//       </figure>
-//       <div class="media-content">
-//         <div class="content">
-//           <p>
-//             <strong>Sean Brown</strong>
-//             <br>
-//             Donec sollicitudin urna eget eros malesuada sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam blandit nisl a nulla sagittis, a lobortis leo feugiat.
-//             <br>
-//             <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
-//           </p>
-//         </div>
-
-//         <article class="media">
-//           Vivamus quis semper metus, non tincidunt dolor. Vivamus in mi eu lorem cursus ullamcorper sit amet nec massa.
-//         </article>
-
-//         <article class="media">
-//           Morbi vitae diam et purus tincidunt porttitor vel vitae augue. Praesent malesuada metus sed pharetra euismod. Cras tellus odio, tincidunt iaculis diam non, porta aliquet tortor.
-//         </article>
-//       </div>
-//     </article>
-
-//     <article class="media">
-//       <figure class="media-left">
-//         <p class="image is-48x48">
-//           <img src="https://bulma.io/images/placeholders/96x96.png">
-//         </p>
-//       </figure>
-//       <div class="media-content">
-//         <div class="content">
-//           <p>
-//             <strong>Kayli Eunice </strong>
-//             <br>
-//             Sed convallis scelerisque mauris, non pulvinar nunc mattis vel. Maecenas varius felis sit amet magna vestibulum euismod malesuada cursus libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus lacinia non nisl id feugiat.
-//             <br>
-//             <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
-//           </p>
-//         </div>
-//       </div>
-//     </article>
-//   </div>
-// </article>
-// <article class="media">
-//   <figure class="media-left">
-//     <p class="image is-64x64">
-//       <img src="https://bulma.io/images/placeholders/128x128.png">
-//     </p>
-//   </figure>
-//   <div class="media-content">
-//     <div class="field">
-//       <p class="control">
-//         <textarea class="textarea" placeholder="Add a comment..."></textarea>
-//       </p>
-//     </div>
-//     <div class="field">
-//       <p class="control">
-//         <button class="button">Post comment</button>
-//       </p>
-//     </div>
-//   </div>
-// </article>

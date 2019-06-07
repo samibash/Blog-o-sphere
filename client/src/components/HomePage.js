edb340c74b2a23c45b0fd8b65b5465921e2e13a7
Sami Bachir <samibachir20@email.com>
2019-06-07 11:44:02 -0400

    styled homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react'
-// import './HomePage.css'
+import './HomePage.css'
 
 export default class HomePage extends Component {
     render() {
         return (
-            <div>
-                <h1>This is the homepage</h1>
-                <div id="homepage">
-                    <img src="https://i.imgur.com/mRPKozg.png" alt ="logo"/>
+            <div>             
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <div class="content">
+                            <img className="img" src="https://i.imgur.com/mRPKozg.png" alt ="logo"/>
+                        </div>
+                    </article>
+                </div>
+
+                <div class="tile is-parent">
+                    <article class="tile is-child box">
+                        <p class="title"><em>“I want to have a platform where my written thoughts on an important issue can have an audience of people who’ll react and comment on them. I think it’s important to further dialogue on such important issues.”</em></p>
+                        <p class="subtitle"> - Anonymous User</p>
+                        <div class="content">
+                        </div>
+                    </article>
                 </div>
             </div>
         )
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import UpdatePostPage from './UpdatePostPage';
 import {Redirect} from 'react-router-dom';
+import './Posts.css'
 
 export default class Posts extends Component {
   
@@ -17,14 +18,14 @@ export default class Posts extends Component {
         console.log(key)
     }
 
-    // handleSubmitUpdate = (key) => {
-    //     axios.put(`http://localhost:3001/posts/${key}`)
-    //         .then(res => {
-    //         console.log(res);
-    //         console.log(res.data);
-    //     })
-    //     console.log(key)
-    // }
+    handleSubmitUpdate = (key) => {
+        axios.put(`http://localhost:3001/posts/${key}`)
+            .then(res => {
+            console.log(res);
+            console.log(res.data);
+        })
+        console.log(key)
+    }
 
     componentDidMount(){
         this.props.upload()
@@ -40,22 +41,111 @@ export default class Posts extends Component {
             const key=post.id
             console.log(key)
             return (
-                <div>
-                    <h2><strong>Title</strong>:{post.title}</h2>
-                    <h5><strong>Genre</strong>: <em>{post.genre}</em></h5>
-                    <p>{post.content}</p>
-                        <button onClick={()=>{this.handleSubmit(key)}}>DELETE</button>
-                        {/* <button onClick={()=>{this.handleSubmitUpdate(key)}}>UPDATE</button> */}
-                        <button>UPDATE</button>
-                </div>
+                <article class="message is-info">
+                    <div class="message-header">
+                        <h2>Title: <strong>{post.title}</strong></h2>
+                        <h5>Genre: <strong><em>{post.genre}</em></strong></h5>
+                        <button class="delete" aria-label="delete" onClick={()=>{this.handleSubmit(key)}}></button>
+                    </div>
+                    <div class="message-body">
+                        <p>{post.content}</p>
+                    </div>
+                    <button onClick={()=>{this.handleSubmitUpdate(key)}}>UPDATE</button>
+                    {/* <button>UPDATE</button> */}
+                </article>
             )
         })
-        
         return (
             <div className="posts">
-                <h1>All Posts</h1>
                 {allPosts}
             </div>
         );
     }
 }
+
+// Comment stuff
+
+
+// <article class="media">
+//   <figure class="media-left">
+//     <p class="image is-64x64">
+//       <img src="https://bulma.io/images/placeholders/128x128.png">
+//     </p>
+//   </figure>
+//   <div class="media-content">
+//     <div class="content">
+//       <p>
+//         <strong>Barbara Middleton</strong>
+//         <br>
+//         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porta eros lacus, nec ultricies elit blandit non. Suspendisse pellentesque mauris sit amet dolor blandit rutrum. Nunc in tempus turpis.
+//         <br>
+//         <small><a>Like</a> · <a>Reply</a> · 3 hrs</small>
+//       </p>
+//     </div>
+
+//     <article class="media">
+//       <figure class="media-left">
+//         <p class="image is-48x48">
+//           <img src="https://bulma.io/images/placeholders/96x96.png">
+//         </p>
+//       </figure>
+//       <div class="media-content">
+//         <div class="content">
+//           <p>
+//             <strong>Sean Brown</strong>
+//             <br>
+//             Donec sollicitudin urna eget eros malesuada sagittis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam blandit nisl a nulla sagittis, a lobortis leo feugiat.
+//             <br>
+//             <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
+//           </p>
+//         </div>
+
+//         <article class="media">
+//           Vivamus quis semper metus, non tincidunt dolor. Vivamus in mi eu lorem cursus ullamcorper sit amet nec massa.
+//         </article>
+
+//         <article class="media">
+//           Morbi vitae diam et purus tincidunt porttitor vel vitae augue. Praesent malesuada metus sed pharetra euismod. Cras tellus odio, tincidunt iaculis diam non, porta aliquet tortor.
+//         </article>
+//       </div>
+//     </article>
+
+//     <article class="media">
+//       <figure class="media-left">
+//         <p class="image is-48x48">
+//           <img src="https://bulma.io/images/placeholders/96x96.png">
+//         </p>
+//       </figure>
+//       <div class="media-content">
+//         <div class="content">
+//           <p>
+//             <strong>Kayli Eunice </strong>
+//             <br>
+//             Sed convallis scelerisque mauris, non pulvinar nunc mattis vel. Maecenas varius felis sit amet magna vestibulum euismod malesuada cursus libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus lacinia non nisl id feugiat.
+//             <br>
+//             <small><a>Like</a> · <a>Reply</a> · 2 hrs</small>
+//           </p>
+//         </div>
+//       </div>
+//     </article>
+//   </div>
+// </article>
+// <article class="media">
+//   <figure class="media-left">
+//     <p class="image is-64x64">
+//       <img src="https://bulma.io/images/placeholders/128x128.png">
+//     </p>
+//   </figure>
+//   <div class="media-content">
+//     <div class="field">
+//       <p class="control">
+//         <textarea class="textarea" placeholder="Add a comment..."></textarea>
+//       </p>
+//     </div>
+//     <div class="field">
+//       <p class="control">
+//         <button class="button">Post comment</button>
+//       </p>
+//     </div>
+//   </div>
+// </article>
